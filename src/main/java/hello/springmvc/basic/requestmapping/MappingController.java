82dc93307b319a733b4f5e32a80562ad8d1ca8f9
eunju <eunju7757@gmail.com>
2022-03-21 22:52:37 +0900

    third commit

--- src/main/java/hello/springmvc/basic/requestmapping/MappingController.java
+++ src/main/java/hello/springmvc/basic/requestmapping/MappingController.java
@@ -32,6 +32,6 @@ public class MappingController {
     @GetMapping("/mapping/users/{userId}/orders/{orderId}")
     public String mappingPath(@PathVariable String userId, @PathVariable Long orderId) {
 
-        return userId + " " + orderId;
+        return userId + " and " + orderId;
     }
 }
